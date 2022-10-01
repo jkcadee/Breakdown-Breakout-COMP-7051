17d6cca258d4e3e374e372fa95b38fefc51907f4
Amber Ma <agmlammy@gmail.com>
2022-09-30 20:07:28 -0700

    Created health bar and health controls for player

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,32 +8,87 @@ public class PlayerControls : MonoBehaviour
 {
     //create private internal references
     private InputActions inputActions;
+
+    //Represents the movment inputaction
     private InputAction movement;
-    Rigidbody rb;
+
+    //Represents the health of the player
+    private float health;
+
+    //Represents the health bar of the player.
+    public GameObject health_meter;
+
+    /** 
+     Sets up the value of the player's health.
+     */
+
+    private void Start() {
+        health = 5.0f;
+        UpdateHealth();
+    }
+
+    /** 
+     * Instantiates the input action object.
+     */
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>(); //get rigidbody, responsible for enabling collision with other colliders
         inputActions = new InputActions(); //create new InputActions
     }
-    //called when script enabled
+
+    /**
+     Enables the movement controls.
+     */
     private void OnEnable()
     {
         movement = inputActions.Player.Movement; //get reference to movement action
         movement.Enable();
     }
-    //called when script disabled
+
+    /**
+        Disables the movement controls.
+     */
     private void OnDisable()
     {
         movement.Disable();
     }
-    //called every physics update
+    /** 
+     Updates the movement as the controls are pressed.
+     */
     private void FixedUpdate()
     {
         Vector2 v2 = movement.ReadValue<Vector2>(); //extract 2d input data
-        Vector3 v3 = new Vector3(v2.x * 0.2f, 0, v2.y); //convert to 3d space
-        //Debug.Log(v2.x);
-        //Debug.Log(v2.y);
+        Vector3 v3 = new Vector3(v2.x * 1.0f, 0, v2.y); //convert to 3d space
         transform.Translate(v3);
+
+    }
+
+    /** 
+     * Updates the health bar in accordance with the player's current health.
+     */
+    private void UpdateHealth() {
+
+        health_meter.GetComponent<RectTransform>().sizeDelta = new Vector2(health*30, 23.9f) ;
+
+    }
+
+    /** 
+     * Decrements the player's health by one.
+    */
+    private void TakeDamage() {
+        health -= 1.0f;
+        UpdateHealth();
+    }
+
+    /** 
+       Increments the player's health by one.
+    */
+    private void Heal() {
+        health += 1.0f;
+        UpdateHealth();
+    }
+
+    void Update() {
 
     }
 }
