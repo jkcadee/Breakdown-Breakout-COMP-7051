f96056fa1f0366e02b2702b80bcca60fd1c0957b
luminaryFlowers <71102109+luminaryFlowers@users.noreply.github.com>
2022-10-01 00:54:39 -0700

    shoot towards mouse position
    => attach SpawnBullet and PlayerShoot to an object to be able to shoot from that player's position
    ===> a ShootTarget and ShootPlane must also be in that scene

--- Assets/Scripts/MousePoint.cs
+++ Assets/Scripts/MousePoint.cs
@@ -9,19 +9,21 @@ public class MousePoint : MonoBehaviour
 
     void Start()
     {
+        // only shoots the ray on the MouseWorld layer
         layerMask = LayerMask.GetMask("MouseWorld");
-        Debug.Log(layerMask);
         mainCam = Camera.main;
     }
 
     void Update()
     {
+        // used this video as reference:
+        // https://www.youtube.com/watch?v=0jTPKz3ga4w
         Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
         if(Physics.Raycast(ray, out RaycastHit raycastHit, 500f, layerMask))
         {
+            // moves THIS OBJECT to the point of the mouse
             transform.position = raycastHit.point;
-            //Debug.Log("haiii");
         }
     }
 }
