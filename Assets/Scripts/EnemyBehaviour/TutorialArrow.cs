64beec27a311c6fe4b5e6be0a79f19d5f0dbe34f
Jay Sui <jsui5@my.bcit.ca>
2022-11-23 20:00:05 -0800

    updated weapon demo level

---
+++ Assets/Scripts/EnemyBehaviour/TutorialArrow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialArrow : MonoBehaviour
+{
+    private float speed = 5f;
+    private float height = 1f;
+    private Vector3 initialPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float newY = Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(initialPos.x, newY * height + initialPos.y, initialPos.z);
+    }
+}
